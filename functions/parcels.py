e9f4a634e2d8a453e420caa243ca99679d03b5e9
traitortots <caleb.thomas.smith@gmail.com>
2023-11-06 18:54:47 -0500

    add attribute setter for polygon edges

--- functions/parcels.py
+++ functions/parcels.py
@@ -10,6 +10,41 @@ class Parcel(Polygon):
         self.address = address
         self.zoning = zoning
 
+        # Extract edges from the exterior ring and store them as LineString objects
+        self.edge_attributes = {}
+        coords = list(self.exterior.coords)
+        self.edges = [LineString([coords[i], coords[i+1]]) for i in range(len(coords) - 1)]
+
+        # Initialize attributes for each edge using WKT as the key
+        for edge in self.edges:
+            self.edge_attributes[edge.wkt] = {}
+
+    def set_edge_attribute(self, edge_index, attribute, value):
+        """Set an attribute to a specific edge."""
+        if edge_index < len(self.edges):
+            self.edge_attributes[self.edges[edge_index].wkt][attribute] = value
+        else:
+            raise IndexError("Edge index out of range.")
+    
+    def get_edge_attribute(self, edge_index, attribute):
+        """Retrieve an attribute from a specific edge."""
+        if edge_index < len(self.edges):
+            return self.edge_attributes[self.edges[edge_index].wkt].get(attribute, None)
+        else:
+            raise IndexError("Edge index out of range.")
+        
+    def get_all_edges_with_attributes(self):
+        """Return a list of dictionaries with edges and their attributes."""
+        edges_info = []
+        for edge in self.edges:
+            edge_info = {
+                'wkt': edge.wkt,
+                'coordinates': list(edge.coords),
+                'attributes': self.edge_attributes[edge.wkt]
+            }
+            edges_info.append(edge_info)
+        return edges_info
+        
     def description(self):
         desc = f"Parcel Details:\n"
         desc += f"  - Owner: {self.owner_name if self.owner_name else 'Unknown'}\n"
